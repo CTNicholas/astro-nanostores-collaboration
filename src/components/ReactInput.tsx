f9e2991f3e796a556132db18b4b68a86966c575d
CTNicholas <33033422+CTNicholas@users.noreply.github.com>
2023-05-29 21:43:12 +0100

    feat: Finished

--- src/components/ReactInput.tsx
+++ src/components/ReactInput.tsx
@@ -1,26 +1,20 @@
 import { useStore } from "@nanostores/react";
 import { inputValue, setInputValue } from "../store";
+import ReactLogo from "./ReactLogo";
 
 export default function ReactInput() {
   const value = useStore(inputValue);
   return (
     <div>
       <label htmlFor="react">
-        <svg viewBox="-11.5 -10.23174 23 20.46348">
-          <title>React Logo</title>
-          <circle cx="0" cy="0" r="2.05" fill="#61dafb" />
-          <g stroke="#61dafb" strokeWidth="1" fill="none">
-            <ellipse rx="11" ry="4.2" />
-            <ellipse rx="11" ry="4.2" transform="rotate(60)" />
-            <ellipse rx="11" ry="4.2" transform="rotate(120)" />
-          </g>
-        </svg>
+        <ReactLogo />
       </label>
       <input
         id="react"
         type="text"
         value={value}
         onChange={(e) => setInputValue(e.target.value)}
+        onClick={(e) => e.currentTarget.select()}
       />
     </div>
   );
